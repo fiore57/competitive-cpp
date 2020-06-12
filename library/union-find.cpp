63bdf048b10fb31ace0f29b8bd5cceb2196c4f95
fiore57 <fiore57.cpp@gmail.com>
2020-06-13 02:42:27 +0900

    Update unionfind

--- library/union-find.cpp
+++ library/union-find.cpp
@@ -1,50 +1,39 @@
-class UnionFind
-{
+/**
+ * @brief UnionFind
+ */
+class UnionFind {
 public:
-    UnionFind(const int n) : par(n), rank(n, 0), sz(n, 1)
-    {
+    UnionFind(const int n) : par(n), rank(n, 0), sz(n, 1) {
         rep(i, n) par[i] = i;
     }
 
-    int find(const int x)
-    {
+    int find(const int x) {
         if (par[x] == x)
             return x;
         return par[x] = find(par[x]);
     }
 
-    void unite(int x, int y)
-    {
+    void unite(int x, int y) {
         x = find(x);
         y = find(y);
         if (x == y)
             return;
 
-        if (rank[x] < rank[y])
-        {
+        if (rank[x] < rank[y]) {
             par[x] = y;
             sz[y] += sz[x];
-        }
-        else
-        {
+        } else {
             par[y] = x;
             sz[x] += sz[y];
-            if (rank[x] == rank[y])
-            {
+            if (rank[x] == rank[y]) {
                 ++rank[x];
             }
         }
     }
 
-    bool same(const int x, const int y)
-    {
-        return find(x) == find(y);
-    }
+    bool same(const int x, const int y) { return find(x) == find(y); }
 
-    int size(const int x)
-    {
-        return sz[find(x)];
-    }
+    int size(const int x) { return sz[find(x)]; }
 
 private:
     vint par, rank, sz;
