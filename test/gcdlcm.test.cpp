e13a4ab6d6ab8dece47917699c68144ef41dcfba
fiore57 <fiore57.cpp@gmail.com>
2020-09-06 22:42:32 +0900

    Add gcdlcm test

---
+++ test/gcdlcm.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM                                                                \
+    "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C"
+// clang-format off
+#include "../_template/_template.cpp"
+#include "../math/gcdlcm.cpp"
+// clang-format on
+
+void Main() {
+    int n = in();
+    vector<int> a = in(n);
+    ll l = a.front();
+    for (const auto e : a) {
+        l = lcm(l, e);
+    }
+    out(l);
+}
