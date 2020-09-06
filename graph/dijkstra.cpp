49b8c6c7daa5bbf8c07a5dbbb62149b70b502bad
Kousuke Takaki <u7693@protonmail.com>
2020-09-06 22:07:58 +0900

    Fix dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,12 +1,10 @@
-template<typename T>
-struct ShortestPath {
-  vector<T> dist;
-  vector<int> from, id;
+template <typename T> struct ShortestPath {
+    vector<T> dist;
+    vector<int> from, id;
 };
 
-template< typename T >
-ShortestPath< T > dijkstra(const Graph<T> &g, int s) {
-    constexpr auto INF = numeric_limits< T >::max();
+template <typename T> ShortestPath<T> dijkstra(const Graph<T> &g, const int s) {
+    constexpr auto INF = numeric_limits<T>::max();
     using Pi = pair<T, int>;
 
     vector<int> from(g.size(), -1), id(g.size(), -1);
@@ -16,25 +14,25 @@ ShortestPath< T > dijkstra(const Graph<T> &g, int s) {
     dist[s] = 0;
     que.emplace(dist[s], s);
 
-    while(!que.empty()) {
+    while (!que.empty()) {
         T cost;
         int idx;
         tie(cost, idx) = que.top();
         que.pop();
 
-        if(dist[idx] < cost)
+        if (dist[idx] < cost)
             continue;
 
-        for(auto &e : g.g[idx]) {
+        for (const auto &e : g.g[idx]) {
             auto next_cost = cost + e.cost;
-            if(dist[e.to] <= next_cost)
+            if (dist[e.to] <= next_cost)
                 continue;
             dist[e.to] = next_cost;
             from[e.to] = idx;
             id[e.to] = e.idx;
             que.emplace(dist[e.to], e.to);
         }
-  }
+    }
 
-  return { dist, from, id };
+    return {dist, from, id};
 }
